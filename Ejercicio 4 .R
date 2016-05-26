427bb895de57006f06522f770d9cfa428205a4b3
GuillermoSandV <memoalf@yahoo.com>
2016-05-26 17:08:58 -0500

    Add files via upload

---
+++ Ejercicio 4 .R
@@ -0,0 +1,40 @@
+###Script para obtener algunos parametros basicos de Genética de poblaciones
+###compilado de Hierfstat latest features de Jerome Goudet (2015-12-03)
+###Archivo de SNP´s tomado del gitHub de Mark Christie
+###Ejercicio 4
+
+library(adegenet)
+library(hierfstat)
+
+reads<-read.delim("../data/SNP_data.txt")
+head(reads)
+
+data(reads) 
+is.genind(reads)
+
+##Cuantos loci hay
+n.loci    <- (ncol(reads)/2)-1    
+n.loci
+head(n.loci)
+
+#cuantos individuos hay por población
+n.per.pop <- table(reads[, 2])   
+n.per.pop
+
+# boxplot of Ho, Hs, Ht
+
+boxplot(basic.stats(reads)$perloc[,1:5]) 
+
+##Estadistica descriptiva
+
+#Riqueza alelica de primeros 20 loci
+
+boxplot(t(allelic.richness(reads)$Ar[1:20,]))
+
+
+#PCA de genotipos principales
+x<-indpca(reads[,-2],ind.labels=reads[,2])
+plot(x,col=reads[,1],cex=0.7)
+
+
+
